5e5cbd13381bbba8897c2890713683474512b07f
kdcllc <info@kingdavidconsulting.com>
2021-08-24 11:27:31 -0400

    Show order status

--- src/BlazingPizza.Client/Program.cs
+++ src/BlazingPizza.Client/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
