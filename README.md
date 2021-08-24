c91637f478385222d469ec8cc3ddd710eaed65a7
kdcllc <info@kingdavidconsulting.com>
2021-08-24 13:24:51 -0400

    Refactor state management

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This is `blazor-workshop` workshop.
 - [01. Components and layout](https://github.com/dotnet-presentations/blazor-workshop/blob/master/docs/01-components-and-layout.md)
 - [02. Customize a pizza](https://github.com/dotnet-presentations/blazor-workshop/blob/master/docs/02-customize-a-pizza.md)
 - [03. Show order status](https://github.com/dotnet-presentations/blazor-workshop/blob/master/docs/03-show-order-status.md)
+- [04. Refactor state management](https://github.com/dotnet-presentations/blazor-workshop/blob/master/docs/04-refactor-state-management.md)
+
 ## Resources
 
 ### Unit/ E2E testing
---
+++ src/BlazingPizza.Client/OrderState.cs
@@ -0,0 +1,52 @@
+﻿namespace BlazingPizza.Client
+{
+    public class OrderState
+    {
+        public bool ShowingConfigureDialog { get; private  set; }
+
+        public Pizza ConfiguringPizza { get; private  set; }
+
+        public Order Order { get; private  set; } = new Order();
+
+        public void ShowConfigurePizzaDialog(PizzaSpecial special)
+        {
+            ConfiguringPizza = new Pizza()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>(),
+            };
+
+            ShowingConfigureDialog = true;
+        }
+
+        public void CancelConfigurePizzaDialog()
+        {
+            ClearState();
+        }
+
+        public void ConfirmConfigurePizzaDialog()
+        {
+            Order.Pizzas.Add(ConfiguringPizza);
+
+            ClearState();
+        }
+
+        public void ResetOrder()
+        {
+            Order = new Order();
+        }
+
+        public void RemoveConfiguredPizza(Pizza pizza)
+        {
+            Order.Pizzas.Remove(pizza);
+        }
+
+        private void ClearState()
+        {
+            ConfiguringPizza = null;
+            ShowingConfigureDialog = false;
+        }
+    }
+}
